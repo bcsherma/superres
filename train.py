f48f1ccdd37f1c10edac8a7e0a36a40e9ece171c
Ben Sherman <bcanfieldsherman@gmail.com>
2022-06-09 12:10:38 -0700

    added cli and wandb config

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import argparse
 from os import path
 
 import pytorch_lightning as pl
@@ -9,29 +10,76 @@ from data import Div2K
 from model import ResidualDenseNetwork
 
 
+def parse_args():
+    parser = argparse.ArgumentParser("train.py")
+    parser.add_argument("dataset", type=str)
+    parser.add_argument("--dl_workers", type=int, default=8)
+    parser.add_argument("--growth_rate", type=int, default=64)
+    parser.add_argument("--log_grad", type=int, default=0)
+    parser.add_argument("--max_epochs", type=int, default=1000)
+    parser.add_argument("--nblocks", type=int, default=16)
+    parser.add_argument("--nchannels", type=int, default=3)
+    parser.add_argument("--nfeatures", type=int, default=64)
+    parser.add_argument("--nlayers", type=int, default=8)
+    parser.add_argument("--patch_size", type=int, default=32)
+    parser.add_argument("--save_code", action="store_true")
+    parser.add_argument("--train_batch_size", type=int, default=8)
+    parser.add_argument("--val_batch_size", type=int, default=1)
+    parser.add_argument("--val_im_size", type=int, default=128)
+    return parser.parse_args()
+
+
 def main():
-    
-    with wandb.init() as run:
 
-        run.log_code()
+    args = parse_args()
 
-        data_source = run.use_artifact("div2k:latest")
+    with wandb.init(job_type="train", config=args) as run:
+
+        config = run.config
+
+        if config.save_code:
+            run.log_code()
+
+        data_source = run.use_artifact(config.dataset)
+        config["scale_factor"] = data_source.metadata["scale_factor"]
+
         data_root = data_source.download()
-        train_lr_dir =  path.join(data_root, "train_lr")
-        train_hr_dir =  path.join(data_root, "train_hr")
-        valid_lr_dir =  path.join(data_root, "valid_lr")
-        valid_hr_dir =  path.join(data_root, "valid_hr")
+        train_lr_dir = path.join(data_root, "train_lr")
+        train_hr_dir = path.join(data_root, "train_hr")
+        valid_lr_dir = path.join(data_root, "valid_lr")
+        valid_hr_dir = path.join(data_root, "valid_hr")
 
-        train_ds = Div2K(4, train_lr_dir, train_hr_dir, size=30)
-        val_ds = Div2K(4, valid_lr_dir, valid_hr_dir, size=90, center=True)
+        train_ds = Div2K(
+            config.scale_factor, train_lr_dir, train_hr_dir, size=config.patch_size
+        )
+        val_ds = Div2K(
+            config.scale_factor,
+            valid_lr_dir,
+            valid_hr_dir,
+            size=config.val_im_size,
+            center=True,
+        )
 
-        train_dl = DataLoader(train_ds, batch_size=8, num_workers=8)
-        val_dl = DataLoader(val_ds, batch_size=1, num_workers=8)
+        train_dl = DataLoader(
+            train_ds, batch_size=config.train_batch_size, num_workers=config.dl_workers
+        )
+        val_dl = DataLoader(
+            val_ds, batch_size=config.val_batch_size, num_workers=config.dl_workers
+        )
 
-        model = ResidualDenseNetwork(64, 64, 16, 8, 3, 4)
-        logger = WandbLogger()
-        logger.watch(model, log_freq=200, log="all")
-        trainer = pl.Trainer(gpus=1, logger=WandbLogger())
+        model = ResidualDenseNetwork(
+            config.nfeatures,
+            config.growth_rate,
+            config.nblocks,
+            config.nlayers,
+            config.nchannels,
+            config.scale_factor,
+        )
+
+        logger = WandbLogger(experiment=run)
+        if config.log_grad:
+            logger.watch(model, log_freq=config.log_grad)
+        trainer = pl.Trainer(gpus=1, logger=logger, max_epochs=config.max_epochs)
         trainer.fit(model, train_dl, val_dl)
 
 
