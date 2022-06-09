96de5b4560f2c370a158ffd0426ed885df1d6aab
Ben Sherman <bcanfieldsherman@gmail.com>
2022-06-09 12:33:37 -0700

    added register_dataset cli

--- register_dataset.py
+++ register_dataset.py
@@ -1,18 +1,42 @@
+import argparse
 import wandb
 
 
+def parse_args():
+    parser = argparse.ArgumentParser("register_dataset.py")
+    parser.add_argument("bucket", type=str)
+    parser.add_argument("scale_factor", type=int, default=4)
+    return parser.parse_args()
+
+
 def main():
-    
-    with wandb.init() as run:
 
-        dataset = wandb.Artifact("div2k", type="image-dataset")
+    args = parse_args()
 
-        dataset.add_reference("gs://wandb-superres-bucket/div2k/DIV2K_valid_HR/", name="valid_hr")
-        dataset.add_reference("gs://wandb-superres-bucket/div2k/DIV2K_train_HR/", name="train_hr")
-        dataset.add_reference("gs://wandb-superres-bucket/div2k/DIV2K_valid_LR_bicubic/X4", name="valid_lr")
-        dataset.add_reference("gs://wandb-superres-bucket/div2k/DIV2K_train_LR_bicubic/X4", name="train_lr")
+    with wandb.init(job_type="register_data", config=args) as run:
 
+        config = run.config
+        dataset = wandb.Artifact(
+            "div2k",
+            type="image-dataset",
+            metadata=dict(scale_factor=config.scale_factor),
+        )
+
+        dataset.add_reference(
+            f"gs://{config.bucket}/div2k/DIV2K_valid_HR/", name="valid_hr"
+        )
+        dataset.add_reference(
+            f"gs://{config.bucket}/div2k/DIV2K_train_HR/", name="train_hr"
+        )
+        dataset.add_reference(
+            f"gs://{config.bucket}/div2k/DIV2K_valid_LR_bicubic/X{config.scale_factor}",
+            name="valid_lr",
+        )
+        dataset.add_reference(
+            f"gs://{config.bucket}/div2k/DIV2K_train_LR_bicubic/X{config.scale_factor}",
+            name="train_lr",
+        )
         run.log_artifact(dataset)
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     main()
